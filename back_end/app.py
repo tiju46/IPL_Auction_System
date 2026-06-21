16d6fba5b9530a6d828467d0b6eef2453b2145c2
tiju46 <pttiju@gmail.com>
2026-06-21 17:46:26 +0100

    app.run() was missing in previous commit

--- back_end/app.py
+++ back_end/app.py
@@ -21,3 +21,9 @@ def login():
     if data["username"] == admin["username"] and data["password"] == admin["password"]:
         return jsonify({"success": True})
     return jsonify({"success": False}), 401
+
+
+
+
+if __name__ == "__main__":
+   app.run(debug=True)
