6b9502b2d290c969c352ba11287c9ebf570181a4
tiju46 <pttiju@gmail.com>
2026-06-25 19:22:35 +0100

    added Cloud Run port on flask app

--- back_end/app.py
+++ back_end/app.py
@@ -84,4 +84,5 @@ def get_teams():
     return jsonify(load_json("teams.json"))
 
 if __name__ == "__main__":
-   app.run(debug=True)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
