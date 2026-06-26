e4d48f32851bb737c93a251b226523e360b658b6
tiju46 <pttiju@gmail.com>
2026-06-26 15:58:57 +0100

    modified loadPlayers(value) to take params also added conditions in displayPlayers()

--- front_end/script.js
+++ front_end/script.js
@@ -26,24 +26,60 @@ function login() {
 
 let allPlayers = []; // store players globally
 // ---------------- LOAD PLAYERS ----------------
-function loadPlayers() {
+function loadPlayers(value) {
     fetch(`${API}/players`)
         .then(res => res.json())
         .then(players => {
             allPlayers = players; // save full list
-            displayPlayers(players);
+            displayPlayers(players, value);
         });
 }
-function displayPlayers(players) {
+function displayPlayers(players, value) {
     const table = document.getElementById("playerTable");
-    table.innerHTML = "";
+    const table1 = document.getElementById("HomeplayerTable");
+    //console.log(table.id);
+    //console.log(value);
+    if (value == "player") {       
+        table.innerHTML = ""; 
+        players.forEach(p => {
+            let roleClass = "role-batsman";
+            if (p.role.toLowerCase().includes("bowl")) roleClass = "role-bowler";
+            if (p.role.toLowerCase().includes("all")) roleClass = "role-allrounder";
 
-    players.forEach(p => {
+            table.innerHTML += `
+                <tr>
+                    <td>${p.id}</td>
+
+                    <td><input id="name_${p.id}" value="${p.name}"></td>
+
+                    <td>
+                        <span class="role-badge ${roleClass}">
+                        ${p.role}
+                        </span>
+                        <input id="role_${p.id}" value="${p.role}" style="display:none;">
+                    </td>
+
+                    <td><input id="price_${p.id}" value="${p.base_price}"></td>
+
+                    <td>${p.team_id ? p.team_id : "None"}</td>
+
+                    <td>
+                        <button class="action-btn update" onclick="updatePlayer(${p.id})">✏️</button>
+                        <button class="action-btn delete" onclick="deletePlayer(${p.id})">🗑️</button>
+                    </td>
+                </tr>
+            `;
+        });
+    }
+    else if (value == "home") {
+        console.log(table1.id);
+        table1.innerHTML = "";
+        players.forEach(p => {
         let roleClass = "role-batsman";
         if (p.role.toLowerCase().includes("bowl")) roleClass = "role-bowler";
         if (p.role.toLowerCase().includes("all")) roleClass = "role-allrounder";
 
-        table.innerHTML += `
+        table1.innerHTML += `
             <tr>
                 <td>${p.id}</td>
 
@@ -59,18 +95,12 @@ function displayPlayers(players) {
                 <td><input id="price_${p.id}" value="${p.base_price}"></td>
 
                 <td>${p.team_id ? p.team_id : "None"}</td>
-
-                <td>
-                    <button class="action-btn update" onclick="updatePlayer(${p.id})">✏️</button>
-                    <button class="action-btn delete" onclick="deletePlayer(${p.id})">🗑️</button>
-                </td>
             </tr>
         `;
     });
+
+    }
 }
-
-
-
 function filterPlayers() {
     const query = document.getElementById("searchBox").value.toLowerCase();
 
